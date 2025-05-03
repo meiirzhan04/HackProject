e218676e4c1b580a5eede38be0cff87045815760
ZeMe1 <manas.esendikov@mail.ru>
2025-05-03 22:32:36 +0500

    button

---
+++ app/src/main/java/com/example/sxodimsduu/login/CreateButton.kt
@@ -0,0 +1,28 @@
+package com.example.sxodimsduu.login
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+@Composable
+fun CustomButton(
+    text: String ,
+    backgroundColor: Color = Color(0xFF_12CDD9)
+) {
+    Button(
+        onClick = {} ,
+        modifier = Modifier.fillMaxWidth() ,
+        colors = ButtonDefaults.buttonColors(
+            containerColor = backgroundColor,
+            contentColor = Color.White
+        )
+    ) {
+        Text(
+            text = text
+        )
+    }
+}
