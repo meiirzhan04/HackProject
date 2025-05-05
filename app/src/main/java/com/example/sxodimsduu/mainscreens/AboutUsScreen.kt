07b6ef20641545c2fb0543d5e643ee769524114f
ZeMe1 <manas.esendikov@mail.ru>
2025-05-05 12:21:29 +0500

    club

--- app/src/main/java/com/example/sxodimsduu/mainscreens/AboutUsScreen.kt
+++ app/src/main/java/com/example/sxodimsduu/mainscreens/AboutUsScreen.kt
@@ -1,6 +1,5 @@
 package com.example.sxodimsduu.mainscreens
 
-import android.R.attr.bottom
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
@@ -9,15 +8,11 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -25,9 +20,6 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.sxodimsduu.R
-import java.text.SimpleDateFormat
-import java.util.Calendar
-import java.util.Date
 
 @Preview
 @Composable
@@ -35,7 +27,7 @@ fun AboutUsScreen() {
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .verticalScroll(rememberScrollState())
+            .background(color = Color(0xFF_1F1D2B))
             .padding(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
@@ -49,7 +41,7 @@ fun AboutUsScreen() {
 
         Text(
             text = "About Us",
-            fontSize = 32.sp,
+            fontSize = 24.sp,
             fontWeight = FontWeight.SemiBold,
             color = Color.White,
         )
